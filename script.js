e4489916f255a581052af7e825092f40c13d0164
Michelle Hokeness <cvaggio@gmail.com>
2021-01-12 11:46:13 -0800

    adding questions for quiz

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ const responseDiv = document.querySelector("#response");
 const endPageEl = document.querySelector(".end-page");
 const initialAndScore = document.querySelector("#staticEmail");
 
+
+// Created questions and put them in an array
+
+const questions = [
+    {
+        title: "Which is considered a function",
+        choices: ["setTimer()", "var headEl = document.querySelector", "()setTimer", ".setAttribute"],
+        answer: "setTimer()",
+    },
+    {
+        title: "What does HTML stand for?",
+        choices: ["HyperText Mode Language", "HyperText Markup Language", "HyperText Modern Language", "HyperText Markup Line"],
+        answer: "HyperText Markup Language",
+    },
+    {
+        title: "When applying forms on your webpage, what should always be added into the code?",
+        choices: ["clearInterval", "event.preventDefault", "the DOM", "querySelector.form"],
+        answer: "event.preventDefault",
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Color Style Sheet", "Cascading Style Sheet", "Contact Style Sheet", "Computer Solving System"],
+
+        answer: "Cascading Style Sheet",
+    },
+    {   
+        title: "When creating an element in JavaScript, which will return an array of elements",
+        choices: ["getElementById(‘idname’)", "getElementsByClass(‘classname’)", 
+        "getElementsByTagName(‘tagname’)", "querySelectorAll()"],
+        answer: "querySelectorAll()",
+    }
+]
