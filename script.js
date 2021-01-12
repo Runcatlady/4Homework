254208a6e2d0060599b385078b418a5ec2d6f81f
Michelle Hokeness <cvaggio@gmail.com>
2021-01-12 11:39:03 -0800

    staging elements to be called

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const startPageEl = document.querySelector(".start-page");
+const timeElement = document.querySelector("#time");
+const wrapperElement = document.querySelector(".wrapper");
+const btnElement = document.querySelector("#start");
+const divContEL = document.querySelector(".divContainer");
+const h3Element = document.querySelector("#title");
+const olEl = document.querySelector("#questlist");
+const finishDiv = document.querySelector(".finish");
+const Score = document.querySelector("#result");
+const errMsg = document.querySelector("#error");
+const initialInput = document.querySelector("#inputInitial").value;
+const submitEl = document.querySelector(".btn btn-primary mb-2");
+const responseDiv = document.querySelector("#response");
+const endPageEl = document.querySelector(".end-page");
+const initialAndScore = document.querySelector("#staticEmail");
+
